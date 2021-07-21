7ece63de91e4d745f27a47af707f5a404cee6653
Jules <peyrat.jules@yahoo.fr>
2021-07-21 11:25:54 +0200

    TODO et packages principaux

---
+++ src/main/java/fr/lezoo/stonks/Stonks.java
@@ -0,0 +1,19 @@
+package fr.lezoo.stonks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Stonks extends JavaPlugin {
+    public static Stonks plugin;
+
+    public void onLoad() {
+        plugin = this;
+    }
+
+    public void onEnable() {
+
+        // Change once plugin is posted on Spigot
+        /*new Metrics(this, 111111);*/
+
+
+    }
+}
