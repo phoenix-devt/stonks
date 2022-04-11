bf0184f99c398c46bc322d8e8da197057a096f8b
Indyuce <cym.peyrat@yahoo.fr>
2022-04-11 17:39:41 +0200

    New placeholders

--- src/main/java/fr/lezoo/stonks/compat/placeholder/StonksPlaceholders.java
+++ src/main/java/fr/lezoo/stonks/compat/placeholder/StonksPlaceholders.java
@@ -2,6 +2,8 @@ package fr.lezoo.stonks.compat.placeholder;
 
 import fr.lezoo.stonks.Stonks;
 import fr.lezoo.stonks.player.PlayerData;
+import fr.lezoo.stonks.quotation.Quotation;
+import fr.lezoo.stonks.share.Share;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
@@ -41,14 +43,31 @@ public class StonksPlaceholders extends PlaceholderExpansion {
         Player player = offlinePlayer.getPlayer();
         PlayerData playerData = PlayerData.get(player);
 
-
-
         if (params.startsWith("shares_")) {
             String id = params.substring(7);
             if (!Stonks.plugin.quotationManager.has(id))
                 return "?";
 
             return Double.toString(playerData.countShares(Stonks.plugin.quotationManager.get(id)));
+        }
+
+        if (params.startsWith("share_money_")) {
+            String id = params.substring(7);
+            if (!Stonks.plugin.quotationManager.has(id))
+                return "?";
+
+            double sum = 0;
+            for (Share share : playerData.getShares(Stonks.plugin.quotationManager.get(id)))
+                sum += share.getCloseEarning(0);
+            return String.valueOf(sum);
+        }
+
+        if (params.startsWith("portfolio_money")) {
+            double sum = 0;
+            for (Quotation quotation : Stonks.plugin.quotationManager.getQuotations())
+                for (Share share : playerData.getShares(quotation))
+                    sum += share.getCloseEarning(0);
+            return String.valueOf(sum);
         }
 
         return "";
